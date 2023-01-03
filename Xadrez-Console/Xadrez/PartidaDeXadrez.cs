d30bddffc3e4739fad90849dc8949c3393679f90
Samuelpj <samueljacobsen@alunos.utfpr.edu.br>
2023-01-03 11:53:15 -0300

    Testes iniciais com movimentos possiveis

--- Xadrez-Console/Xadrez/PartidaDeXadrez.cs
+++ Xadrez-Console/Xadrez/PartidaDeXadrez.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using tabuleiro;
+using Xadrez;
 
-namespace Xadrez
+namespace xadrez
 {
     class PartidaDeXadrez
     {
+
         public Tabuleiro tab { get; private set; }
         private int turno;
         private Cor jogadorAtual;
@@ -17,9 +17,10 @@ namespace Xadrez
             tab = new Tabuleiro(8, 8);
             turno = 1;
             jogadorAtual = Cor.Branca;
+            terminada = false;
             colocarPecas();
-
         }
+
         public void executaMovimento(Posicao origem, Posicao destino)
         {
             Peca p = tab.retirarPeca(origem);
@@ -27,6 +28,7 @@ namespace Xadrez
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
         }
+
         private void colocarPecas()
         {
             tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 1).toPosicao());
--- Xadrez-Console/tabuleiro/Peca.cs
+++ Xadrez-Console/tabuleiro/Peca.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 
-
 namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
+
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
-        public int qtdMovimentos { get; protected set; }
+        public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Tabuleiro tabuleiro, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
             this.posicao = null;
             this.tab = tab;
             this.cor = cor;
-            this.qtdMovimentos = 0;
+            this.qteMovimentos = 0;
         }
         public void incrementarQteMovimentos()
         {
-            qtdMovimentos++;
+            qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
